8ee62e4ff838386e588b149f86cab7c22eff02c9
m93a <m93a.cz@gmail.com>
2022-12-16 14:17:43 +0100

    compression

--- src/fetchFromCache.ts
+++ src/fetchFromCache.ts
@@ -1,5 +1,22 @@
+import { deflate, inflate } from "pako";
 import { getListFromAPI, ItemType, type Item } from "./fetchFromAPI";
 import { PromiseWithProgress } from "./promiseWithProgress";
+
+function serialize(value: any): string {
+  const json = JSON.stringify(value);
+  const defl = deflate(json);
+  const bufr = Buffer.from(defl);
+  const serl = bufr.toString("base64");
+  return serl;
+}
+
+function deserialize(serl: string): any {
+  const bufr = Buffer.from(serl, "base64");
+  const defl = new Uint8Array(bufr);
+  const json = inflate(defl, { to: "string" });
+  const value = JSON.parse(json);
+  return value;
+}
 
 const CACHE_KEY_AUTHORS = "CACHE_KEY_AUTHORS";
 const CACHE_KEY_WORKS = "CACHE_KEY_WORKS";
@@ -16,14 +33,10 @@ export function getList<T extends ItemType>(
   const cachedValue = localStorage.getItem(key);
 
   if (cachedValue !== null) {
-    return PromiseWithProgress.fromValue(
-        JSON.parse(cachedValue)
-    );
+    return PromiseWithProgress.fromValue(deserialize(cachedValue));
   } else {
     let listPromise = getListFromAPI(t);
-    listPromise.then((list) =>
-      localStorage.setItem(key, JSON.stringify(list))
-    );
+    listPromise.then((list) => localStorage.setItem(key, serialize(list)));
     return listPromise;
   }
 }
